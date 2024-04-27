9293b5569fe26c2ede3383320e46386cfcecaf4b
sarthak <sarthakgaud04@gmail.com>
2024-04-27 23:41:25 +0530

    added some requests

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,30 @@
 import express from 'express';
-
+import bodyParser from "body-parser";
 import blockchain from "../blockchain/blockchain";
 
 const app = express();
 const PORT = process.env.PORT || 3210;
 
-const bc = new blockchain();
+app.use(bodyParser.json())
+
+const bc: blockchain = new blockchain();
 
 app.get('/blockchain' , (req , res) => {
     res.json(bc.chain)
+})
+
+app.post('/blockchain/mine' , (req , res) => {
+    const block: any = req.body.data
+
+    if(!block) {
+        res.status(400).send('Block not found')
+        return
+
+    }
+
+    bc.addBlock(block)
+
+    res.redirect('/blockchain')
 })
 
 app.listen(PORT, () => {
